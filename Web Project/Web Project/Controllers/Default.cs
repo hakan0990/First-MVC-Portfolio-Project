2b62c59cdfd24a41ef465835cbca42e91dd6546d
hakan0990 <hakandemirel0990@hotmail.com>
2024-05-12 15:22:21 +0300

    First MVC Portfolio Project

---
+++ Web Project/Web Project/Controllers/Default.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Project.Controllers
+{
+    public class Default : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
